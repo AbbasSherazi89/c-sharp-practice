36a70a2638cf80250cbb8f4636be6844e2074f04
AbbasSherazi89 <ibne.abbassherazi89@gmail.com>
2026-05-13 09:51:03 +0300

    prac: add two tasks for exercise only

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,38 +8,53 @@
 //string result = $"My name is {name2} and I am {age} years old.";
 //Console.WriteLine(result);
 
-int[] arr = new int[4] { 1, 5, 3, 2 };
-Array.Sort(arr);
-foreach(int i in arr)
-{
-    Console.WriteLine(i);
-}
-Console.WriteLine(arr.Max());
+//int[] arr = new int[4] { 1, 5, 3, 2 };
+//Array.Sort(arr);
+//foreach(int i in arr)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine(arr.Max());
 
-int[,] numbers = { { 1, 4, 2 }, { 5, 4, 0 } };
+//int[,] numbers = { { 1, 4, 2 }, { 5, 4, 0 } };
 
-foreach(int i in numbers)
-{
-    Console.WriteLine("\n");
-    Console.WriteLine(i);
-}
+//foreach(int i in numbers)
+//{
+//    Console.WriteLine("\n");
+//    Console.WriteLine(i);
+//}
 
 //Exercise 1:Print all even numbers from 1 to 50 using a loop.
-Console.WriteLine("Even numbers from 1 to 50: \n");
-for (int i=0; i<=50; i++)
+//Console.WriteLine("Even numbers from 1 to 50: \n");
+//for (int i=0; i<=50; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+////Exercise 2: Take user input and reverse it without using a arra.reverse method.
+
+// Console.WriteLine("Enter a string to reverse:");
+// string userInput = Console.ReadLine();
+
+//for (int i=userInput.Length - 1; i >= 0; i-- )
+//{
+//            Console.Write(userInput[i]);
+//}
+
+//Exercise 3: Create an array of 5 numbers and calculate their sum and average.
+
+int[] sum_averaged_arr = new int[5]{ 45, 22, 44, 55, 22 };
+int sum = 0;
+for (int i=0; i<sum_averaged_arr.Length; i++)
 {
-    if (i % 2 == 0)
+    sum+= sum_averaged_arr[i];
+    if (i == sum_averaged_arr.Length - 1)
     {
-        Console.WriteLine(i);
+        Console.WriteLine("Sum of the array is:" + sum);
+        Console.WriteLine("Average of the array is:" + (sum / sum_averaged_arr.Length));
     }
 }
 
-//Exercise 2: Take user input and reverse it without using a arra.reverse method.
-
- Console.WriteLine("Enter a string to reverse:");
- string userInput = Console.ReadLine();
-
-for (int i=userInput.Length - 1; i >= 0; i-- )
-{
-            Console.Write(userInput[i]);
-}
